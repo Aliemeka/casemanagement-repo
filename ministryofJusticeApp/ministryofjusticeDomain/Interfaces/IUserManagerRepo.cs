272c2b6408b00d1e9b60de8294eb353309f0cd1a
Chukwuemeka Allison <c.allison@genesystechhub.com>
2021-02-09 13:46:06 +0100

    Added admin controller

---
+++ ministryofJusticeApp/ministryofjusticeDomain/Interfaces/IUserManagerRepo.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using ministryofjusticeDomain.Entities;
+
+namespace ministryofjusticeDomain.Interfaces
+{
+    public interface IUserManagerRepo
+    {
+        IEnumerable<ApplicationUser> GetAllUsers();
+        void AssignRole(ApplicationUser user, string roleName);
+    }
+}
