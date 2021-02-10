084c399c1c8521d47d58311b48778ad514568a2f
Emeka Allison <emekaallison4@gmail.com>
2021-02-10 15:48:04 +0000

    Added user management and role management

---
+++ ministryofJusticeApp/ministryofjusticeDomain/Repositories/UnitOfWork.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using ministryofjusticeDomain.Entities;
+using ministryofjusticeDomain.Interfaces;
+
+namespace ministryofjusticeDomain.Repositories
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly ApplicationDbContext _context;
+
+        public IDepartmentRepo DepartmentRepo { get; }
+        public IUserManagerRepo UserManagerRepo { get; }
+
+        public UnitOfWork(ApplicationDbContext context,
+            IDepartmentRepo departmentRepo,
+            IUserManagerRepo userManagerRepo)
+        {
+            _context = context;
+            DepartmentRepo = departmentRepo;
+            UserManagerRepo = userManagerRepo;
+        }
+    }
+}
